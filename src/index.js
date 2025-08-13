7980dc3f5233c1c9cb4bdf2c0891f17be64ee17c
AkkRepo <98938565+AkkRepo@users.noreply.github.com>
2025-08-13 12:56:50 +0530

    Bug Fix

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="">
+    {/* <BrowserRouter basename="/namma_booking_admin"> */}
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
